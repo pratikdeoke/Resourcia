09b2ae88cf154897fe6d9a940501a443e13ddde0
pratikdeoke <deokepratik3@gmail.com>
2026-01-29 10:13:13 +0530

    validate.middleware added

---
+++ backend/src/middlewares/validate.middleware.js
@@ -0,0 +1,26 @@
+import { ZodError } from 'zod'
+
+export const validate = (schema) => {
+  return (req, res, next) => {
+    try {
+      schema.parse({
+        body: req.body,
+        params: req.params,
+        query: req.query
+      })
+      next()
+    } catch (err) {
+      if (err instanceof ZodError) {
+        return res.status(400).json({
+          success: false,
+          message: 'Validation failed',
+          errors: err.errors.map((e) => ({
+            field: e.path.join('.'),
+            message: e.message
+          }))
+        })
+      }
+      next(err)
+    }
+  }
+}
