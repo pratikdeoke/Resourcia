79ecabd7093f67c3fd9a94ebf08c96b6af5a6a7f
pratikdeoke <deokepratik3@gmail.com>
2026-01-29 10:07:21 +0530

    auth.schema added

---
+++ backend/src/validations/auth.schema.js
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const registerSchema = z.object({
+  body: z.object({
+    organization_id: z.string().uuid(),
+    name: z.string().min(2),
+    email: z.string().email(),
+    password: z.string().min(8)
+  })
+})
+
+export const loginSchema = z.object({
+  body: z.object({
+    email: z.string().email(),
+    password: z.string().min(1)
+  })
+})
