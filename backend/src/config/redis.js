776c20f519a1fecafc96b230b661858e73a5d26d
pratikdeoke <deokepratik3@gmail.com>
2026-02-05 00:46:59 +0530

    changes done

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -2,6 +2,7 @@ import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
   url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
 });
 
 export default redis;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,23 @@
 import "dotenv/config";
 import app from "./app.js";
 import pool from "./config/db.js";
+import { de } from "zod/v4/locales";
 
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 
-const startServer = async () => {
-  try {
-    await pool.query("SELECT 1");
-    console.log("Database connected");
+// const startServer = async () => {
+//   try {
+//     await pool.query("SELECT 1");
+//     console.log("Database connected");
 
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server", error);
-    process.exit(1);
-  }
-};
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Failed to start server", error);
+//     process.exit(1);
+//   }
+// };
 
-startServer();
+// startServer();
+export default app;
