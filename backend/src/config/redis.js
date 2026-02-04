afafa7161ce789e9538c7f34997da8c5c39a8977
pratikdeoke <deokepratik3@gmail.com>
2026-02-05 00:57:59 +0530

    changes done

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,5 +4,6 @@ const redis = new Redis({
   url: process.env.REDIS_URL,
   token: process.env.REDIS_TOKEN,
 });
-
+console.log("REDIS_URL exists:", !!process.env.REDIS_URL);
+console.log("REDIS_TOKEN exists:", !!process.env.REDIS_TOKEN);
 export default redis;
