30e81ec27181afb04c20a92c605e93e498c125a3
pratikdeoke <deokepratik3@gmail.com>
2026-02-08 18:00:31 +0530

    app.js modified

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,14 +5,19 @@ import routes from "./routes/index.js";
 const app = express();
 
 app.use(cors({
-    origin: 'https://resourcia-frontend.netlify.app',
-    credentials: true,
+  origin: "https://resourcia-frontend.netlify.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use("/api/v1", routes);
 
 app.get("/", (req, res) => {
-    res.send("Backend is running");
-})
+  res.send("Backend is running");
+});
 
 export default app;
