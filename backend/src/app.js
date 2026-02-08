1457b220f8697799c76349372c3a91c28eec1e58
pratikdeoke <deokepratik3@gmail.com>
2026-02-09 00:03:11 +0530

    app.js modified for testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
+import dns from 'dns';
 
 const app = express();
 
@@ -10,6 +11,13 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use("/api/v1", routes);
+
+app.get('/test-dns', (req, res) => {
+  dns.lookup(process.env.DB_HOST, (err, address) => {
+    if (err) return res.status(500).send(err.message);
+    res.send(`DB resolves to ${address}`);
+  });
+});
 
 app.get("/", (req, res) => {
     res.send("Backend is running");
